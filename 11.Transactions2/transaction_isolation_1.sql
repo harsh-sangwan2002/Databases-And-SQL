6740ebbed72fb1ef5e7f1cc8a504f04cabc7906a
harsh-sangwan2003 <hsangwan2002@gmail.com>
2024-07-19 14:07:39 +0530

    minor changes

--- 11.Transactions2/transaction_isolation_1.sql
+++ 11.Transactions2/transaction_isolation_1.sql
@@ -1,13 +1,13 @@
 use sakila;
 
 select * from film
-where film_id = 10;
+where film_id = 2;
 
 -- This change is also reflecting in another transaction
 -- By default, for evey query level mysql has autocommit set to true 
 update film
-set title = "The Demon King"
-where film_id = 10;
+set title = "something"
+where film_id = 2;
 
 set autocommit = false;
 set autocommit = true;
@@ -19,4 +19,4 @@ commit;
 
 show variables like 'transaction_isolation';
 
-set session transaction isolation level read committed; 
+set session transaction isolation level read committed;
--- 11.Transactions2/transaction_isolation_2.sql
+++ 11.Transactions2/transaction_isolation_2.sql
@@ -1,17 +1,22 @@
 use sakila;
 
 select * from film
-where film_id = 10;
+where film_id = 2;
 
 update film
 set title = "Godzilla"
-where film_id = 10;
+where film_id = 20;
 
 set autocommit = false;
-set autocommit = true;
 
 commit;
 
 show variables like 'transaction_isolation';
 
-set session transaction isolation level read committed;
+set session transaction isolation level read committed; 
+
+/*
+For the first time when you read a row, it creates a snapshot of the rows.
+It will keep reading the same value in the current transaction
+*/
+set session transaction isolation level repeatable read;
